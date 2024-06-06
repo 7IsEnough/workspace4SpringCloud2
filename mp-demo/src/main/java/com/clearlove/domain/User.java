6c898b7be3b6314ed7d9b70f0f21c40b2bbc6003
Chen Zhenhuan <976949689@qq.com>
2024-06-07 00:54:22 +0800

    2024年6月7日00:54:18

--- mp-demo/src/main/java/com/clearlove/domain/User.java
+++ mp-demo/src/main/java/com/clearlove/domain/User.java
@@ -1,20 +1,27 @@
 package com.clearlove.domain;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@TableName("user")
 public class User {
 
     /**
      * 用户id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
      * 用户名
      */
+    @TableField("`username`")
     private String username;
 
     /**
